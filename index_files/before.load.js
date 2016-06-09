7ec128f11b1877d57a13f701ce41762733c517c1
xiaoshun.xu <xiaoshun.xu@operasolutions.com>
2016-06-08 22:30:42 -0700

    fix js error

--- index_files/before.load.js
+++ index_files/before.load.js
@@ -4,31 +4,31 @@ $(document).ready(function($) {
 
     var $body = $('body');
 
-    if( $body.hasClass('navigation-top-header') ) {
-        $( ".main-navigation.navigation-top-header" ).load( "assets/external/_navigation.html" );
-    }
-    else if( $body.hasClass('navigation-off-canvas') ) {
-        $( ".main-navigation.navigation-off-canvas" ).load( "assets/external/_navigation.html" );
-    }
-    mobileNavigation();
+    // if( $body.hasClass('navigation-top-header') ) {
+    //     $( ".main-navigation.navigation-top-header" ).load( "assets/external/_navigation.html" );
+    // }
+    // else if( $body.hasClass('navigation-off-canvas') ) {
+    //     $( ".main-navigation.navigation-off-canvas" ).load( "assets/external/_navigation.html" );
+    // }
+    //mobileNavigation();
     toggleNav();
 });
 
-$(window).resize(function(){
-    mobileNavigation();
-});
+// $(window).resize(function(){
+//     mobileNavigation();
+// });
 
 // Navigation on small screen ------------------------------------------------------------------------------------------
 
-function mobileNavigation(){
-    if( $(window).width() < 979 ){
-        $(".main-navigation.navigation-top-header").remove();
-        $(".toggle-navigation").css("display","inline-block");
-        $(".main-navigation.navigation-off-canvas").load("assets/external/_navigation.html");
-        $("body").removeClass("navigation-top-header");
-        $("body").addClass("navigation-off-canvas");
-    }
-}
+// function mobileNavigation(){
+//     if( $(window).width() < 979 ){
+//         $(".main-navigation.navigation-top-header").remove();
+//         $(".toggle-navigation").css("display","inline-block");
+//         $(".main-navigation.navigation-off-canvas").load("assets/external/_navigation.html");
+//         $("body").removeClass("navigation-top-header");
+//         $("body").addClass("navigation-off-canvas");
+//     }
+// }
 
 // Toggle off canvas navigation ----------------------------------------------------------------------------------------
 
